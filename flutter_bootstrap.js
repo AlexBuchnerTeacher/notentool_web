161a3e97e69fc53b8dc82e898e490592df6764fc
Alex Buchner <alex@example.com>
2025-11-21 06:55:54 +0100

    Update GitHub Pages with correct base-href

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"b5990e5ccc5e325fd24f0746e7d6689bbebc7c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2181575820"
+    serviceWorkerVersion: "3832675831"
   }
 });
